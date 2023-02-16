3d5ffddfc6e2bf4d40c46449fba85b2c6bac98d1
melita2 <melitana401@hotmail.co.uk>
2023-02-16 12:18:43 +0000

    assign fruits to word_list

---
+++ Practicals_m2.py
@@ -0,0 +1,27 @@
+#%%
+"Hello World"
+#%%
+print("hasan")
+#%%
+x =7
+y = 8
+print(x+y)
+
+import sys
+
+text = sys.stdin.read()
+num_chars = len(text)
+
+print(f"The number of characters in the text is: {num_chars}")
+
+import sys
+
+text = sys.stdin.read()
+num_chars = len(text)
+
+print(f"The number of characters in the text is: {num_chars}")
+import os
+
+my_var_value = os.environ.get('MY_VAR')
+print(f'The value of MY_VAR is {my_var_value}')
+
---
+++ milestone_2.py
@@ -0,0 +1,3 @@
+#%%
+word_list = ["apple","grapes","mango","strawberry","pineapple"]
+print(word_list)
