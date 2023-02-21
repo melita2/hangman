77e4e84db7e8b0a3147ec8846382c980f7375c21
melita2 <melitana401@hotmail.co.uk>
2023-02-21 21:28:40 +0000

    join files to see if it will verify on milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -3,3 +3,18 @@ word_list = ["apple", "pineapple", "grapes", "strawberry", "mango"]
 word = random.choice(word_list)
 print(word)
 guess = input("Please enter a single letter: ")
+
+
+
+import random
+
+word_list = ["apple", "pineapple", "grapes", "strawberry", "mango"]
+word = random.choice(word_list)
+print(word)
+
+guess = input("Please enter a single letter: ")
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! this is not valid input.")
