f3be9da344d28a62499e2ca8de22157faa6fd23f
melita2 <melitana401@hotmail.co.uk>
2023-02-21 12:59:56 +0000

    add check len is 1 and good guess and oops

--- milestone_2.3.py
+++ milestone_2.3.py
@@ -1,4 +1,12 @@
 import random
+
 word_list = ["apple", "pineapple", "grapes", "strawberry", "mango"]
 word = random.choice(word_list)
+print(word)
+
 guess = input("Please enter a single letter: ")
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
