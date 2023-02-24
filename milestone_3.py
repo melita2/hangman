64317846b183f431c9aaf470364f0a5ee62789cc
melita2 <melitana401@hotmail.co.uk>
2023-02-24 13:38:29 +0000

    update without return

--- milestone_3.py
+++ milestone_3.py
@@ -15,10 +15,8 @@ def check_guess(guess, word):
     guess = guess.lower()
     if guess in word:
         print(f"Good guess! {guess} is in the word.")
-        return True
     else:
         print(f"Sorry, {guess} is not in the word. Try again.")
-        return False
 
 def ask_for_input():
     while True:
@@ -27,7 +25,6 @@ def ask_for_input():
             return guess.lower()
         else:
             print("Invalid input! Please enter a single letter.")
-
 while True:
     guess = ask_for_input()
     if check_guess(guess, word):
