36842a613ef66aeb9752e11b3d0377ac4884e22c
melita2 <melitana401@hotmail.co.uk>
2023-02-24 13:29:07 +0000

    add all the code in one code

--- milestone_3.py
+++ milestone_3.py
@@ -1,29 +1,25 @@
-while True:
-    guess = input("Guess a letter: ")
-    if guess.isalpha() and len(guess) == 1:
-        break
-    else:
-        print("Invalid letter, enter a single alphabetical character.")
+import random
 
+word_list = ["apple", "pineapple", "grapes", "strawberry", "mango"]
+word = random.choice(word_list)
+print(word)
 
-word = "apple"
-guess = input("Guess a letter: ")
+guess = input("Please enter a single letter: ")
 
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
 else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
-
-
+    print("Oops! this is not valid input.")
 
 def check_guess(guess, word):
     guess = guess.lower()
     if guess in word:
-        print("Correct!")
+        print(f"Good guess! {guess} is in the word.")
         return True
     else:
-        print("Incorrect!")
+        print(f"Sorry, {guess} is not in the word. Try again.")
         return False
+
 def ask_for_input():
     while True:
         guess = input("Guess a letter: ")
@@ -31,6 +27,8 @@ def ask_for_input():
             return guess.lower()
         else:
             print("Invalid input! Please enter a single letter.")
-            
-guess = ask_for_input()
-check_guess(guess, "apple")
+
+while True:
+    guess = ask_for_input()
+    if check_guess(guess, word):
+        break
