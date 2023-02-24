4bd5cb3da757d8cf0e3916ec9034d09be7eafd59
melita2 <melitana401@hotmail.co.uk>
2023-02-24 10:20:37 +0000

    add check if the letter is in word

--- milestone_3.py
+++ milestone_3.py
@@ -4,3 +4,12 @@ while True:
         break
     else:
         print("Invalid letter, enter a single alphabetical character.")
+
+
+word = "apple"
+guess = input("Guess a letter: ")
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
