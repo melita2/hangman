5f0cd6adcdd9b4c3f07fa89d5879ac2a84cb4972
melita2 <melitana401@hotmail.co.uk>
2023-02-24 12:53:11 +0000

    add cheack if two letters are correct in the word

--- milestone_3.py
+++ milestone_3.py
@@ -13,3 +13,24 @@ if guess in word:
     print(f"Good guess! {guess} is in the word.")
 else:
     print(f"Sorry, {guess} is not in the word. Try again.")
+
+
+
+def check_guess(guess, word):
+    guess = guess.lower()
+    if guess in word:
+        print("Correct!")
+        return True
+    else:
+        print("Incorrect!")
+        return False
+def ask_for_input():
+    while True:
+        guess = input("Guess a letter: ")
+        if len(guess) == 1 and guess.isalpha():
+            return guess.lower()
+        else:
+            print("Invalid input! Please enter a single letter.")
+            
+guess = ask_for_input()
+check_guess(guess, "apple")
