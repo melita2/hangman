9fcc54f3290d80b87125aa02f756d15ee35daf15
melita2 <melitana401@hotmail.co.uk>
2023-03-19 23:45:26 +0000

    add check guess and ask input

--- milestone_4.py
+++ milestone_4.py
@@ -1,10 +1,38 @@
 import random 
 
 class Hangman:
-  def __init__(self, word_list, num_lives=5):
-    self.word = random.choice(word_list)
-    self.word_guessed = ['_' for letter in self.word]
-    self.num_letters = len(set(self.word))
-    self.num_lives = num_lives
-    self.word_list = word_list
-    self.list_of_guesses = []
+    def __init__(self, word_list, num_lives=5):
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_' for letter in self.word]
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+            for i, letter in enumerate(self.word):
+                if guess == letter:
+                    self.word_guessed[i] = guess
+            self.num_letters -= 1
+
+    def ask_for_input(self):
+        while True:
+            guess = input("Guess a letter: ")
+            if not guess.isalpha() or len(guess) != 1:
+                print("Invalid letter. Please enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                if guess not in self.word:
+                    print(f"Sorry, {guess} is not in the word.")
+                self.list_of_guesses.append(guess)
+                self.check_guess(guess)
+                if self.num_letters == 0:
+                    break
+
+
+
+   
