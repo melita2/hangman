0b2b787dd148457cd46084931470d40984a60341
melita2 <melitana401@hotmail.co.uk>
2023-03-19 21:59:30 +0000

    add class remove word list

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -1,34 +1,3 @@
-import random
-
-def check_guess(guess):
-    guess = guess.lower()
-    if guess in word:
-        print(f"Good guess! {guess} is in the word.")
-    else:
-        print(f"Sorry, {guess} is not in the word. Try again.")
-
-def ask_for_input(word):
-    while True:
-        guess = input("Guess a letter: ")
-        if len(guess) == 1 and guess.isalpha():
-            check_guess(guess)
-            break
-        else:
-            print("Invalid input! Please enter a single letter.")
-
-word_list = ["apple", "pineapple", "grapes", "strawberry", "mango"]
-word = random.choice(word_list)
-print(word)
-
-guess = input("Please enter a single letter: ")
-
-if len(guess) == 1 and guess.isalpha():
-    print("Good guess!")
-else:
-    print("Oops! this is not valid input.")
-
-ask_for_input(word)
-
 class Hangman:
     
     def __init__(self, word_list, num_lives=5):
