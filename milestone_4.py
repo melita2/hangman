d8a8a942255a58cc69c141ad43357de9ce1247d3
melita2 <melitana401@hotmail.co.uk>
2023-03-20 22:49:19 +0000

    sorry is not in the world and reduced num of lives

--- milestone_4.py
+++ milestone_4.py
@@ -9,14 +9,18 @@ class Hangman:
         self.word_list = word_list
         self.list_of_guesses = []
 
-    def check_guess(self, guess):
-        guess = guess.lower()
-        if guess in self.word:
-            print(f"Good guess! {guess} is in the word.")
-            for i, letter in enumerate(self.word):
-                if guess == letter:
-                    self.word_guessed[i] = guess
-            self.num_letters -= 1
+def check_guess(self, guess):
+    guess = guess.lower()
+    if guess in self.word:
+        print(f"Good guess! {guess} is in the word.")
+        for i, letter in enumerate(self.word):
+            if guess == letter:
+                self.word_guessed[i] = guess
+        self.num_letters -= 1
+    else:
+        print(f"Sorry, {letter} is not in the word.")
+        self.num_lives -= 1
+        print(f"You have {self.num_lives} lives left.")
 
 
 
